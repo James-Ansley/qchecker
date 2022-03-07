c917f105282c61164d8525cc5e2fd782d2385f3c
James Ansley <ansleyjpf@gmail.com>
2022-03-07 13:58:34 +1300

    added fix for colorama bug

--- src/qchecker/general/_process.py
+++ src/qchecker/general/_process.py
@@ -13,4 +13,7 @@ def _run_subprocess(args: list[str], stdin: str) -> bytes:
             stderr=subprocess.PIPE,
     ) as process:
         stdout, _ = process.communicate(stdin.encode(), timeout=3)
+    # Resolves issue with Colorama in PyCharm running subprocesses
+    # https://github.com/tartley/colorama/issues/263
+    stdout = stdout.removesuffix(b"\x1b[0m")
     return stdout
