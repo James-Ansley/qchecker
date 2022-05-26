f4a7baf751f479e51f76e7f8a9fc932d5491fe41
James Ansley <ansleyjpf@gmail.com>
2022-05-26 15:21:28 +1200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -39,6 +39,7 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 autosummary_generate = True
+autodoc_mock_imports = ["pylint"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
