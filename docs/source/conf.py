0f2d1e8517f4d3eecd3950957889f0b0bb0cf7b9
James Ansley <ansleyjpf@gmail.com>
2022-05-26 15:01:25 +1200

    updating docs?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2022, James Ansley'
 author = 'James Ansley'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.2'
 
 # -- General configuration ---------------------------------------------------
 
